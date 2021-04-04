498fe75f83f321d7a9c50dacc43636bee521b989
Mitch <MitchelClark1997@outlook.com>
2021-04-05 08:07:31 +1000

    adding elligible flights list

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,9 +5,16 @@ class FlightsController < ApplicationController
         [a.airport_code, a.airport_code]
       end
     end
+    @airport_options.uniq
     @passengers_options = [['1', 1], ['2', 2], ['3', 3], ['4',4]]
     @flights = Flight.all
-    @flight_options = Flight.all.map {|f| [f.start_time_formatted, f.start_time]}
+    @flight_options = Flight.all.map {|f| [f.start_time_formatted, f.start_time_formatted]}
+    @elligible = []
+    @flights.each do |flight|
+      if params[:start_code] == flight.start_code && params[:to_airport] == flight.end_code && params[:start_time] == flight.start_time_formatted
+        @elligible.push(flight)
+      end
+    end
   end
 
   def show
